b197c1d7a737b3aea58e4a4c7e9ac643b718793d
Bydo <gamedevbydo@gmail.com>
2024-12-09 00:52:06 -0300

    Ajuste nos audios

--- TCC_ProjectArena/Assets/Scripts/AnimatorForChildren.cs
+++ TCC_ProjectArena/Assets/Scripts/AnimatorForChildren.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AnimatorForChildren : MonoBehaviour
 {
@@ -19,5 +20,14 @@ public class AnimatorForChildren : MonoBehaviour
     public void _SpawnBomb()
     {
         if(player.IsOwner) player.SpawnBombProjectile_ServerRpc();
+    }
+
+    public AudioSource sfxSource;
+
+    public void PlaySFX()
+    {
+        sfxSource.Stop();
+        sfxSource.pitch = Random.Range(0.8f, 1.2f);
+        sfxSource.Play();
     }
 }
